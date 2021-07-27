c3d9d2c133aaca02911a6b3280f301ef6059f02c
Кошарный Никита <ntkst73@mail.ru>
2021-07-27 16:44:10 +0400

    create component

---
+++ src/app/modules/pages/order/components/order-model/order-model.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-model',
+  templateUrl: './order-model.component.html',
+  styleUrls: ['./order-model.component.scss']
+})
+export class OrderModelComponent {
+
+}
